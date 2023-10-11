5ce3e6fdec71580fe54c83e781410a4a6e68f051
Ajay Prakash <21376002@pondiuni.ac.in>
2023-10-11 16:39:21 +0530

    Revert "Delete item list and local Storage"
    This reverts commit a5e41e288c8a84c3726517d5767055dfbf43c892.

--- script.js
+++ script.js
@@ -20,29 +20,19 @@ function onSubmit(e) {
   function li() {
     let li = document.createElement("li");
     li.className = "item-list";
-    let delBtn = document.createElement("button");
-    delBtn.className = "item-del";
 
     li.appendChild(
       document.createTextNode(`${obj.name} ${obj.email} ${obj.phone}`)
     );
-    li.appendChild(delBtn);
-    delBtn.appendChild(document.createTextNode("Delete"));
     itemList.appendChild(li);
-    delBtn.addEventListener("click", DelItem);
-    function DelItem(e) {
-      let li = e.target.parentElement;
-      itemList.removeChild(li);
-      localStorage.removeItem(obj.email);
-    }
   }
-
   function locStorage() {
     obj.name = inputName.value;
     obj.email = inputEmail.value;
     obj.phone = inputPhone.value;
     localStorage.setItem(obj.email, JSON.stringify(obj));
   }
+
   li();
   locStorage();
 }
